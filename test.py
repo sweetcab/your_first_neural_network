ece437e7ddf882e2dc6d116231a060c2894360e0
sweetcab <gaoyang0323@gmail.com>
2017-07-28 14:38:34 +0200

    some other files

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 10:56:15 2017
+
+@author: ygao
+"""
+
+import numpy as np
+
+a = np.array([[0.5, -0.2, 0.1]])
+b = np.array([[1, 2, 3]])
+c= np.random.normal(scale=1 / 3 ** .5,size=8)
+d=np.array([1, 2, 3])
+e=b.T
